e425c76e823f7ce26e8b58389445f59039664822
qiu <188660634@qq.com>
2023-11-13 00:17:56 +0800

    添加商品类目管理 运用组合模式和访问者模式 引入redis 添加商品类

--- src/main/java/com/qiushop/qiushopbackend/QiuShopBackendApplication.java
+++ src/main/java/com/qiushop/qiushopbackend/QiuShopBackendApplication.java
@@ -2,8 +2,10 @@ package com.qiushop.qiushopbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class QiuShopBackendApplication {
 
     public static void main(String[] args) {
